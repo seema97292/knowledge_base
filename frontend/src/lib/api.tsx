ce3b81b6eb23f7e3eb6a29eefef4108aa98518fe
Seema Shah <seemashah9729231968@gmail.com>
2025-06-30 18:47:31 +0530

    add env config for frontend

--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
-const API_BASE_URL = "http://localhost:5101/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5101/api";
 
 // Create axios instance
 const api = axios.create({
